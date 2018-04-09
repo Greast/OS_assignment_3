5cdd276b073c8e424cc8d1531141a1cc4e8c704a
Jeff Gyldenbrand Skov Joergensen <jegyl16@imada-106311.imada.sdu.dk>
2018-04-09 15:16:07 +0200

    .gitignore added

---
+++ src/.tmp_versions/dm510_dev.mod
@@ -0,0 +1,3 @@
+/home/jegyl16/Desktop/fourth_semester/DM501-OS/assignments/OS_assignment_3/src/dm510_dev.ko
+/home/jegyl16/Desktop/fourth_semester/DM501-OS/assignments/OS_assignment_3/src/dm510_dev.o
+
--- src/dm510_dev.mod.c
+++ src/dm510_dev.mod.c
@@ -3,15 +3,16 @@
 #include <linux/compiler.h>
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
 
-struct module __this_module
+__visible struct module __this_module
 __attribute__((section(".gnu.linkonce.this_module"))) = {
- .name = KBUILD_MODNAME,
- .init = init_module,
+	.name = KBUILD_MODNAME,
+	.init = init_module,
 #ifdef CONFIG_MODULE_UNLOAD
- .exit = cleanup_module,
+	.exit = cleanup_module,
 #endif
- .arch = MODULE_ARCH_INIT,
+	.arch = MODULE_ARCH_INIT,
 };
 
 static const char __module_depends[]
